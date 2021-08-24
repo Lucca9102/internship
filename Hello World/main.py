92cd91c5e60a14656c8f0a65e0663dd7f27639e5
Lucca9102 <luccachina@163.com>
2021-08-24 16:57:27 +0800

    finish tutorial

---
+++ Hello World/main.py
@@ -0,0 +1,15 @@
+# # Modules
+# import converters
+# # from converters import kg_to_lbs
+#
+# a = converters.lbs_to_kg(20)
+# b = converters.kg_to_lbs(9)
+# print(a)    # 9.0
+# print(b)    # 20.0
+
+# Packages
+import ecommerce.shipping
+# from ecommerce import shipping
+# from ecommerce.shipping import calc_shipping
+
+ecommerce.shipping.calc_shipping()
